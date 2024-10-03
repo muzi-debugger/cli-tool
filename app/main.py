833ecf810eefc7cfc887ffaae0f78dae73ffaf41
Muzi <mmzolo@dearx.co.za>
2024-10-03 13:21:15 +0200

    Added changes

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
 import typer
 from rich import print
 
+existing_usernames = ["rick", "morty"]
 
 
-def main(name: str, lastname: str ="", formal: bool = False):
-     """
-    Say hi to NAME, optionally with a --lastname.
+def maybe_create_user(username: str):
+    if username in existing_usernames:
+        print("[bold red] The user already exists [/bold red]")
+        raise typer.Abort()
+    else:
+        print(f"[bold green] The user {username} was created [/bold green]")
 
-    If --formal is used, say hi very formally.
-    """
-     if formal:
-        print(f"Good day Mr. {name} {lastname}.")
-     else:
-        print(f"[bold red]Hello[/bold red]  {name} {lastname}! :boom:")
+
+def send_new_user_notification(username: str):
+    # Somehow send a notification here for the new user, maybe an email
+    print(f"Notification sent for new user: {username}")
+
+
+def main(username: str):
+    maybe_create_user(username=username)
+    send_new_user_notification(username=username)
 
 
 if __name__ == "__main__":
