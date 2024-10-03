e44ea5949464674bf70b93757c4927c10d451db3
Muzi <mmzolo@dearx.co.za>
2024-10-03 14:04:02 +0200

    prompts for name and last name

--- app/main.py
+++ app/main.py
@@ -1,23 +1,10 @@
 import typer
-from typing_extensions import Annotated
-
 
 def main(
-    name: Annotated[str, typer.Argument(help="Who to greet")],
-    
-    lastname: Annotated[
-        str, typer.Argument(help="The last name", rich_help_panel="Secondary Arguments")
-    ] = "",
-    age: Annotated[
-        str,
-        typer.Argument(help="The user's age", rich_help_panel="Secondary Arguments"),
-    ] = "",
+    name: str = typer.Option(..., prompt="What is your first name?"),
+    lastname: str = typer.Option(..., prompt="What is your last name?")
 ):
-    """
-    Add a name argument and an optional lastname argument.
-    """
-    print(f"Hello {name} {lastname} you are {age} years old")
-
+    print(f"Hello {name} {lastname}")
 
 if __name__ == "__main__":
     typer.run(main)
